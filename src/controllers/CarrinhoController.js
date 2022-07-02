ce13c0ff49658addbb2a726ddf18b89adb65ac34
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-07-01 22:05:58 -0300

    Finalizado a feature de carrinho com session

--- src/controllers/CarrinhoController.js
+++ src/controllers/CarrinhoController.js
@@ -1,6 +1,48 @@
 const CarrinhoController = {
     showCart: (req, res) => {
-        return res.render('home/carrinho');
+        const {carrinho} = req.session;
+
+        let total = 0;
+
+        if(!carrinho) {
+            return res.render("home/carrinho", {carrinho: [], total})
+        }
+
+        carrinho.forEach(produto => {
+            total += Number(produto.preco);
+        });
+
+        return res.render('home/carrinho', {carrinho, total});
+    },
+
+    addCart: (req, res) => {
+        const { productId, nome, preco, imagem } = req.body;
+
+        const produto = {id: productId, nome, preco, imagem};
+
+        if(req.session.carrinho){
+            req.session.carrinho.push(produto)
+        } else {
+            req.session.carrinho = [produto]
+        }
+        return res.redirect('/carrinho');
+    },
+
+    removeCart: (req, res) => {
+        const {id} = req.params;
+        let {carrinho} = req.session;
+
+        const index = carrinho.findIndex(produto => produto.id == id);
+
+        const carrinhoAtualizado = carrinho.splice(index, 1);
+        carrinho = carrinhoAtualizado;
+
+        if(carrinho.length <= 0) {
+            carrinho = [];
+            return res.redirect('/carrinho')
+        }
+
+        return res.redirect('/carrinho');
     }
 }
 
--- src/routers/carrinhoRouter.js
+++ src/routers/carrinhoRouter.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const CarrinhoController = require('../controllers/CarrinhoController');
+const isLogin = require('../middlewares/isLogin');
 
+router.use(isLogin);
 router.get('/carrinho', CarrinhoController.showCart);
+router.post('/carrinho/adicionar', CarrinhoController.addCart);
+router.delete('/carrinho/:id/remover', CarrinhoController.removeCart);
 module.exports = router;
